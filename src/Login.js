f5849475a4cdf5ccab74e6f9ec1c1440ce69ec29
SamForbey <commanderforber@gmail.com>
2022-11-17 00:55:51 +0000

    Login Page Designed

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { firebase } from '../FirebaseConfig'
-import { StyleSheet, Text, View, TouchableOpacity, TextInput,  } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image  } from 'react-native'
 import Registration from './Registration'
+import { AiTwoToneMail } from "react-icons/ai";
+import TypeWriter from 'react-native-typewriter';
+
 
 const Login = () => {
     const navigation = useNavigation()
@@ -18,16 +21,22 @@ const Login = () => {
     }
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.appbar1}>
-                Login
-            </Text>
-            <View style={{marginTop:40}}>
+            <View style={styles.container}>
+
+            <View style={styles.stretch1}>  
+               <Image style={styles.stretch2} source={require('../assets/LoginImages/QuizAppLogo.png')} />
+            </View> 
+
+            <TypeWriter style={styles.typewriter} typing={1}>6129COMP</TypeWriter>
+            <TypeWriter style={styles.typewriter} typing={1}>Mobile Quiz Application</TypeWriter>
+
+{/*            <View style={{marginTop:20}}>
                 <TextInput
                     style={styles.textInput}
                     placeholder="Email"
                     onChangeText={(email) => setEmail(email)}
                     autoCapitalize="none"
+                    icon="mail"
                     autoCorrect={false}
                 />
                 <TextInput
@@ -39,6 +48,38 @@ const Login = () => {
                     secureTextEntry={true}
                 />                
             </View>
+*/}
+
+            <View style={styles.SectionStyle}>
+            <Image
+                source={require('../assets/LoginImages/IconEmail.png')} 
+                style={styles.ImageStyle}
+            />
+            <TextInput
+                style={{ flex: 1 }}
+                placeholder="Email"
+                onChangeText={(email) => setEmail(email)}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={false}
+             />             
+            </View>
+
+            <View style={styles.SectionStyle}>
+            <Image
+                source={require('../assets/LoginImages/IconPassword.png')} 
+                style={styles.ImageStyle}
+            />
+            <TextInput
+                style={{ flex: 1 }}
+                placeholder="Password"
+                onChangeText={(password) => setPassword(password)}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry={true}
+             />             
+            </View>            
+
             <TouchableOpacity
                 onPress={() => loginUser(email, password)}
                 style = {styles.button}
@@ -49,7 +90,7 @@ const Login = () => {
                 onPress={() => navigation.navigate('registration')}
                 style = {{marginTop:20}}
             >
-                <Text style={{fontWeight:'bold', fontSize:16}}>Register</Text>
+                <Text style={{fontWeight:'bold', fontSize:16}}>Click Here To Register</Text>
             </TouchableOpacity>
         </View>
     )    
@@ -63,7 +104,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        marginTop: 100,
+        marginTop: 0,
+        backgroundColor: 'yellow'
+    },
+
+    stretch1: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    
+    stretch2: {
+        width: 320,
+        height: 200,
+        margin: 10,
+        resizeMode: '',
+    },
+
+    typewriter: {
+        fontSize: 20,
+        fontFamily: 'Courier',
     },
 
     textInput: {
@@ -71,25 +130,48 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         width: 400,
         fontSize: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#000',
+        //borderBottomWidth: 1,
+       // borderBottomColor: '#000',
         marginBottom: 10,
         textAlign: 'center',
+        borderRadius: 10,
+        backgroundColor: "#1df26b",
     },
 
     button: {
         marginTop: 20,
         height: 70,
         width: 250,
-        backgroundColor: '#026EFD',
+        //backgroundColor: '#026EFD',
+        backgroundColor: '#268001',
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 50, 
     },
 
-    appbar1: {
+    appText: {
         fontWeight: 'bold',
         fontSize: 26,
+    },
+
+    SectionStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+        borderWidth: 1,
+        borderColor: '#000',
+        height: 50,
+        borderRadius: 5,
+        margin: 10,
+    },
+    ImageStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+        alignItems: 'center',
     },
 
 })
---
+++ src/Quiz.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import {firebase} from '../FirebaseConfig'
+
+
+const Quiz = () => {
+
+    question: string;
+    answers: string[answer];
+    userAnswer: any;
+
+    const [loading, setLoading] = useState(false);  
+    const [questions, setQuestions] = useState([]); 
+    const [number, setNumber] = useState(0);    
+    const [userAnswers, setUserAnswers] = useState([]);    
+
+
+
+const fetchQuizQuestions = async () => {
+    firebase.collection("objectList").get().then((querySnapshot) => {
+        querySnapshot.forEach(element => {
+            var data = element.data();
+            setInfo(arr => [...arr, data]);
+            //console.log(data);
+            });
+     })
+    }
+
+
+
+}
+
+
+
+
+/* export const fetchQuizQuestions = async ( name, img_dir ) => {
+    (() => {
+        await firebase.firestore().collection('objectList')
+    })
+    await firebase.firestore().collection('objectList')
+
+} */
+
+
+export default Quiz
+
+const styles = StyleSheet.create({})
